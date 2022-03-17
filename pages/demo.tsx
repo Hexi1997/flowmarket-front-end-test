db046807306136a1c28a719c226975e61e668edb
牛叠谊 <18715156450@163.com>
2022-03-17 14:36:54 +0800

    feat: add eslint prettier husky and lint-staged

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,24 +1,23 @@
-import { GetStaticPropsContext, NextPage } from "next";
-import { useTranslation } from "react-i18next";
+import { GetStaticPropsContext, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+// eslint-disable-next-line node/no-extraneous-import
+import { useTranslation } from 'react-i18next';
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale || '', ['common'])),
-        },
-    };
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || '', ['common']))
+    }
+  };
 }
 const Demo: NextPage = () => {
-    const { t } = useTranslation('common');
-    return (
-        <div>
-            <span>{t('toggleLanguage')}</span>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
-    )
-}
+  const { t } = useTranslation('common');
+  return (
+    <div>
+      <span>{t('toggleLanguage')}</span>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
-export default Demo
+export default Demo;
