e23669ea4340aaf41397d0941fb63a3ba22dc08a
牛叠谊 <18715156450@163.com>
2022-03-17 16:06:23 +0800

    feat: support scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,8 +6,15 @@ module.exports = {
     },
     overrides: [
       {
-        files: ['**/*.less'],
-        customSyntax: 'postcss-less',
+        files: ['**/*.scss'],
+        customSyntax: 'postcss-scss',
+        rules: {
+          'prettier/prettier': true
+        }
+      },
+      {
+        files: ['**/*.sass'],
+        customSyntax: 'postcss-sass',
         rules: {
           'prettier/prettier': true
         }
--- pages/demo.tsx
+++ pages/demo.tsx
@@ -2,6 +2,8 @@ import { GetStaticPropsContext, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'react-i18next';
 
+import styles from '@/styles/Demo.module.scss';
+
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
     props: {
@@ -9,10 +11,11 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
     }
   };
 }
+
 const Demo: NextPage = () => {
   const { t } = useTranslation('common');
   return (
-    <div>
+    <div className={styles.container}>
       <span>{t('toggleLanguage')}</span>
       <h1 className="text-3xl font-bold underline border-2 border-orange-500">
         Hello world!
