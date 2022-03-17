4aa55415c0081c6913303c851d1090af55726da1
牛叠谊 <18715156450@163.com>
2022-03-17 12:12:44 +0800

    feat: add tailwind css

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,17 +1,24 @@
-import { GetStaticProps, GetStaticPropsContext, NextPage } from "next";
+import { GetStaticPropsContext, NextPage } from "next";
 import { useTranslation } from "react-i18next";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export async function getStaticProps({ locale }:GetStaticPropsContext) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale||'', ['common'])),
-    },
-  };
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale || '', ['common'])),
+        },
+    };
 }
 const Demo: NextPage = () => {
-    const {t} = useTranslation('common');
-    return <div>{t('toggleLanguage')}</div>
+    const { t } = useTranslation('common');
+    return (
+        <div>
+            <span>{t('toggleLanguage')}</span>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
 }
 
 export default Demo
