cb106890a8bd36e15084649c0413530657fd8d4b
牛叠谊 <18715156450@163.com>
2022-03-21 18:28:18 +0800

    feat: finish user info static

--- components/Header/UserInfo/UserInfo.tsx
+++ components/Header/UserInfo/UserInfo.tsx
@@ -5,10 +5,15 @@ import React, { useState } from 'react';
 
 import infoImg from '@/assets/images/header/admin.svg';
 import collectionsImg from '@/assets/images/header/collections.svg';
+import collectionsWhiteImg from '@/assets/images/header/collections_white.svg';
 import favoritesImg from '@/assets/images/header/favorites.svg';
+import favoritesWhiteImg from '@/assets/images/header/favorites_white.svg';
 import profileImg from '@/assets/images/header/profile.svg';
+import profileWhiteImg from '@/assets/images/header/profile_white.svg';
 import settingsImg from '@/assets/images/header/settings.svg';
+import settingsWhiteImg from '@/assets/images/header/settings_white.svg';
 import signOutImg from '@/assets/images/header/signout.svg';
+import signOutWhiteImg from '@/assets/images/header/signout_white.svg';
 import { Button } from '@/components/Common/Button';
 import { RoundedContainer } from '@/components/RoundedContainer';
 
@@ -21,21 +26,25 @@ interface UserInfoProps {
 const links = [
   {
     icon: profileImg,
+    icon_white: profileWhiteImg,
     title: 'Profile',
     link: '/profile'
   },
   {
     icon: collectionsImg,
+    icon_white: collectionsWhiteImg,
     title: 'My Collections',
     link: '/mycollections'
   },
   {
     icon: favoritesImg,
+    icon_white: favoritesWhiteImg,
     title: 'My Favorites',
     link: '/myfavorites'
   },
   {
     icon: settingsImg,
+    icon_white: settingsWhiteImg,
     title: 'Settings',
     link: '/settings'
   }
@@ -43,7 +52,9 @@ const links = [
 
 export function UserInfo(props: UserInfoProps) {
   const { className } = props;
+  const [hoverSubMenuTitle, setHoverSubMenuTitle] = useState('');
   const [isShowUserInfo, setIsShowUserInfo] = useState(false);
+  const [isHoverSignOut, setIsHoverSignOut] = useState(false);
 
   return (
     <div className={cn(styles.UserInfo, className)}>
@@ -57,7 +68,7 @@ export function UserInfo(props: UserInfoProps) {
         }}
       >
         <Image src={infoImg} className="h-5 w-5" />
-        {true && (
+        {isShowUserInfo && (
           <RoundedContainer className="absolute top-16 -left-40 flex flex-col text-base">
             <span className="pt-6 pb-2 px-[18px]">0x88b4b153184...7c1bd</span>
             <div className="rounded-lg shadow-xl flex p-5 flex-col space-y-2 pb-3 mx-[18px]">
@@ -83,12 +94,28 @@ export function UserInfo(props: UserInfoProps) {
               {links.map((item) => (
                 <Link href={item.link} key={item.title}>
                   <a
+                    onMouseEnter={() => {
+                      setHoverSubMenuTitle(item.title);
+                    }}
+                    onMouseLeave={() => {
+                      setHoverSubMenuTitle('');
+                    }}
                     className={cn(
                       styles.icon,
-                      'flex w-full h-10 items-center px-[18px] space-x-3 hover:bg-themeGreen hover:text-white'
+                      'flex w-full h-10 items-center px-[18px] space-x-3',
+                      hoverSubMenuTitle === item.title
+                        ? 'bg-themeGreen text-white'
+                        : ''
                     )}
                   >
-                    <Image src={item.icon} width="16" />
+                    <Image
+                      src={
+                        item.title === hoverSubMenuTitle
+                          ? item.icon_white
+                          : item.icon
+                      }
+                      width="16"
+                    />
                     <span>{item.title}</span>
                   </a>
                 </Link>
@@ -100,11 +127,21 @@ export function UserInfo(props: UserInfoProps) {
                 console.log('sign out');
               }}
             >
-              <div className="flex w-full h-12 items-center px-[18px] space-x-3 hover:bg-themeGreen hover:text-white">
+              <div
+                onMouseEnter={() => {
+                  setIsHoverSignOut(true);
+                }}
+                onMouseLeave={() => {
+                  setIsHoverSignOut(false);
+                }}
+                className={cn(
+                  'flex w-full h-12 items-center px-[18px] space-x-3',
+                  isHoverSignOut ? 'bg-themeGreen text-white' : ''
+                )}
+              >
                 <Image
-                  src={signOutImg}
+                  src={isHoverSignOut ? signOutWhiteImg : signOutImg}
                   width="16"
-                  className="hover:fill-red-300"
                 />
                 <span>Sign Out</span>
               </div>
